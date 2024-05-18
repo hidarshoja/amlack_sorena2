cf3748c4ccb60acbffc4b6929fa658599b771793
hidarshoja <hidarshoja@gmail.com>
2024-05-18 07:51:24 +0330

    a to router-link

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,41 +3,50 @@
     <div
       class="flex flex-col lg:flex-row items-center justify-center lg:justify-between relative w-[90%]  mx-auto bg-gray-100 shadow-lg py-2 px-3 rounded-xl"
     >
+
       <div
         class="absolute top-[-25px] left-1/2 transform -translate-x-1/2 bg-green-500 w-12 h-12 p-1 rounded-full cursor-pointer"
       >
-        <a href="/">
+         <router-link to="/">
           <span class="inline-block p-1 rounded-full">
             <img src="/icons/home.png" class="w-7" alt="" />
           </span>
-        </a>
+        </router-link>
+       
       </div>
       <div class="flex items-center justify-between w-full lg:w-1/6 gap-5 mt-5 lg:mt-0">
-        <a href="/filing">
+        <router-link to="/filing">
+        
           <div class="flex flex-col items-center gap-1 cursor-pointer">
             <span class="inline-block p-1 rounded-full">
               <img src="/icons/files.png" class="w-7" alt="" />
             </span>
             <span> فایلینگ </span>
           </div>
-        </a>
+      </router-link>
+     
        
-        <a href="/ApplicantsView">
-          <div class="flex flex-col items-center gap-1 cursor-pointer">
-            <span class="inline-block p-1 rounded-full">
-              <img src="/icons/users.png" class="w-7" alt="" />
-            </span>
-            <span> متقاضیان </span>
-          </div>
-        </a>
-        <a href="/ApplicantsView">
+       <router-link to="/ApplicantsView">
+         
             <div class="flex flex-col items-center gap-1 cursor-pointer">
               <span class="inline-block p-1 rounded-full">
                 <img src="/icons/users.png" class="w-7" alt="" />
               </span>
-              <span> مویدی </span>
+              <span> متقاضیان </span>
             </div>
-          </a>
+
+       </router-link>
+      
+       <router-link to="/ApplicantsView">
+         
+         <div class="flex flex-col items-center gap-1 cursor-pointer">
+           <span class="inline-block p-1 rounded-full">
+             <img src="/icons/users.png" class="w-7" alt="" />
+           </span>
+           <span> متقاضیان </span>
+         </div>
+
+    </router-link>
       </div>
       <div></div>
       <div class="flex items-center justify-between w-full lg:w-1/6 gap-5">
